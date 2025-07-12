9f9802fdfb9d90c5a777c3f2b330570df308c77c
Tanmay <tanmay213@gmail.com>
2025-07-12 15:24:02 +0530

    Minor Fixes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, resetPassword, signup } from '../controllers/user.controller.js';
+import { login, resetPassword, signup } from '../contoller/user.controllers.js';
 
 const router = express.Router();
 
